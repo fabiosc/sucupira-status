b6e2efa61a76de00a93e88f7e02534d3da41a579
Fabio Sicupira Cavalcante <sc.fabio@gmail.com>
2019-11-05 02:11:12 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Ping from 'ping-monitor';
+
+const monitors = [];
+const urls = [];
 
 function App() {
+
+  var websites = [
+    {
+      url: "www.google.com",
+      interval: 1
+    }
+  ];
+
+  websites.forEach(function (website) {
+
+    let monitor = new Ping ({
+      website: website.url,
+      interval: website.interval
+    });
+
+    monitor.on('up', function (res) {
+        console.log('Yay!! ' + res.website + ' is up.');
+    });
+
+
+    monitor.on('down', function (res) {
+      console.log('down \n' + 
+       'time       : ' + res.time + '\n' + 
+       'status code: ' + res.statusCode + '\n' +
+       'latency    : ' + res.latency);
+    });
+
+
+    monitor.on('error', function (res) {
+      console.log('error ' + res.website);
+    });
+
+
+    monitor.on('stop', function (website) {
+        console.log(website + ' monitor has stopped.');
+    });
+
+
+    urls.push(website.url);
+    monitors.push(monitor);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
